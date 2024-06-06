acd094528897c715b5b86330c34244ab85fe15ca
Davi Gasparino do Nascimento <davi.gnascimento@sp.senac.br>
2024-06-06 14:52:42 -0300

    v2 Layout base

--- src/Main-v2.py
+++ src/Main-v2.py
@@ -4,6 +4,28 @@ from classes.Templates import Templates as tp
 class App:
     def __init__(self, root):
         self.tp = tp(root)
+        self.menu()
+        #self.tp.Message("Mensagem que va iaparecer na tela")
+    
+    def menu(self):
+        paddingY = 4
+        ctk.CTkButton(
+            self.tp.sidebarMD,
+            width=self.tp.sidebarWidht,
+            text="Principal"
+        ).pack(pady=paddingY)
+        ctk.CTkButton(
+            self.tp.sidebarMD,
+            width=self.tp.sidebarWidht,
+            text="Procedures"
+        ).pack(pady=paddingY)
+        ctk.CTkButton(
+            self.tp.sidebarMD,
+            width=self.tp.sidebarWidht,
+            text="Settings"
+        ).pack(pady=paddingY)
+
+        
         
         
 if __name__ == "__main__":
--- src/classes/Templates.py
+++ src/classes/Templates.py
@@ -6,69 +6,131 @@ class Templates:
     tpH = 35
     tpMD = 355
     ftH = 50
-    padding = 15
+    padding = 20
+    sidebarWidht = 180
+    centerWidht = 500
+    rightWidht = 240
 
     def __init__(self, root):
         self.root = root
         self.root.title("DCID Keypress BOT")
-        self.root.geometry("980x500")
+        self.root.geometry("1000x520")
         self.root.resizable(False, False) 
+        
+        mode = ctk.get_appearance_mode()
 
-        #self.root._set_appearance_mode("light")
+        if mode == 'Light':
+            self.bgSecondary = "#e7e7e7"
         
         self.brandTp = ctk.CTkFrame(
             self.root, 
-            width=180,
+            width=self.sidebarWidht,
             height=self.tpH,
             fg_color=self.bgSecondary
-        ).place( x=self.padding, y=self.padding )
+        )
+        self.brandTp.place( 
+            x=self.padding, 
+            y=self.padding 
+        )
 
         self.centerTP = ctk.CTkFrame(
             self.root,
-            width=500,
+            width=self.centerWidht,
             height=self.tpH,
             fg_color=self.bgSecondary
-        ).place(x=self.padding+180+self.padding, y=self.padding)
+        )
+        self.centerTP.place(
+            x=self.padding+self.sidebarWidht+self.padding, 
+            y=self.padding
+        )
 
         self.msg = ctk.CTkFrame(
             self.root,
-            width=755,
+            width=self.centerWidht,
             height=12,
-            fg_color=self.bgSecondary
-        ).place(x=self.padding+180+self.padding, y=self.padding+36)
+            fg_color="transparent"
+        )
+        self.msg.place(
+            x=self.padding+self.sidebarWidht+self.padding, 
+            y=self.padding+32
+        )
 
         self.rightTP = ctk.CTkFrame(
             self.root,
-            width=240,
+            width=self.rightWidht,
             height=self.tpH,
             fg_color=self.bgSecondary
-        ).place(x=self.padding+695+self.padding, y=self.padding)
+        )
+        self.rightTP.place(
+            x=self.padding+self.sidebarWidht+self.padding+self.centerWidht+self.padding,  
+            y=self.padding
+        )
 
         self.sidebarMD = ctk.CTkFrame(
             self.root,
-            width=180,
+            width=self.sidebarWidht,
             height=self.tpMD,
             fg_color=self.bgSecondary
-        ).place(x=self.padding, y=self.padding+self.tpH+self.padding)
+        )
+        self.sidebarMD.place(
+            x=self.padding, 
+            y=self.padding+self.tpH+self.padding
+        )
+
+        self.rightMD = ctk.CTkFrame(
+            self.root,
+            width=self.rightWidht,
+            height=self.tpMD,
+            fg_color=self.bgSecondary
+        )
+        self.rightMD.place(
+            x=self.padding+self.sidebarWidht+self.padding+self.centerWidht+self.padding, 
+            y=self.padding+self.tpH+self.padding
+        )
 
         self.containerMD = ctk.CTkScrollableFrame(
             self.root,
-            width=730,
-            height=self.tpMD-20,
+            width=self.centerWidht-23,
+            height=self.tpMD-12,
             fg_color=self.bgSecondary
-        ).place(x=self.padding+180+self.padding, y=self.padding+self.tpH+self.padding)
+        )
+        self.containerMD.place(
+            x=self.padding+self.sidebarWidht+self.padding, 
+            y=self.padding+self.tpH+self.padding
+        )
 
         self.sidebarFT = ctk.CTkFrame(
             self.root,
-            width=180,
+            width=self.sidebarWidht,
             height=self.ftH,
             fg_color=self.bgSecondary
-        ).place(x=self.padding, y=self.padding+self.tpH+self.padding+self.tpMD+self.padding)
+        )
+        self.sidebarFT.place(
+            x=self.padding, 
+            y=self.padding+self.tpH+self.padding+self.tpMD+self.padding
+        )
 
         self.containerFT = ctk.CTkFrame(
             self.root,
-            width=755,
+            width=self.centerWidht,
             height=self.ftH,
             fg_color=self.bgSecondary
-        ).place(x=self.padding+180+self.padding, y=self.padding+self.tpH+self.padding+self.tpMD+self.padding)
+        )
+        self.containerFT.place(
+            x=self.padding+self.sidebarWidht+self.padding, 
+            y=self.padding+self.tpH+self.padding+self.tpMD+self.padding
+        )
 
+        self.rightFT = ctk.CTkFrame(
+            self.root,
+            width=self.rightWidht,
+            height=self.ftH,
+            fg_color=self.bgSecondary
+        )
+        self.rightFT.place(
+            x=self.padding+self.sidebarWidht+self.padding+self.centerWidht+self.padding,   
+            y=self.padding+self.tpH+self.padding+self.tpMD+self.padding
+        )
+
+    def Message(self, text):
+        ctk.CTkLabel(self.msg, text=text, font=("Arial", 12, "bold")).pack(pady=0, padx=0)
